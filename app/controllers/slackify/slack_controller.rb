423ee3e7f5659f52bab319876d1349a5219e694b
Justin Léger <hey@justinleger.ca>
2020-04-19 05:07:20 -0400

    fix `exlude` -> `exclude` typo

--- app/controllers/slackify/slack_controller.rb
+++ app/controllers/slackify/slack_controller.rb
@@ -62,7 +62,7 @@ module Slackify
       return if (params[:event][:subtype] == "bot_message" ||
         params[:event].key?(:bot_id) ||
         params[:event][:hidden]) &&
-                Slackify.configuration.whitelisted_bot_ids.exlude?(params.dig(:event, :bot_id))
+                Slackify.configuration.whitelisted_bot_ids.exclude?(params.dig(:event, :bot_id))
 
       command = params[:event][:text]
       Slackify::Router.call_command(command, params[:slack])
